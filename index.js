a31ec581a68615ade3ff7f26fc4c393920bcb8cc
Lucien Casteres <lucien@rendementlocatif.com>
2022-04-12 10:10:21 +0200

    add test case

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-import { MeiliSearch } from 'meilisearch'
+import { MeiliSearch } from "meilisearch";
 
 const actions = [
-    {
-        id: Math.floor(Math.random() * 1000),
-        name: 'search',
-        description: 'Search for a term',
-    },
-    {
-        id: Math.floor(Math.random() * 1000),
-        name: 'add',
-        description: 'Add a new document',
-    },
-    {
-        id: Math.floor(Math.random() * 1000),
-        name: 'delete',
-        description: 'Delete a document',
-    },
-    {
-        id: Math.floor(Math.random() * 1000),
-        name: 'update',
-        description: 'Update a document',
-    },
-    {
-        id: Math.floor(Math.random() * 1000),
-        name: 'list',
-        description: 'List all documents',
-    },
-]
-const userId = '2'
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "search",
+    description: "Search for a term",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "add",
+    description: "Add a new document",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "delete",
+    description: "Delete a document",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "delete",
+    description: "Delete a tenant",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "delete",
+    description: "Delete a customer",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "delete",
+    description: "Delete a bs",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "update",
+    description: "Update a document",
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    name: "list",
+    description: "List all documents",
+  },
+];
+const userId = "2";
 
-const client = new MeiliSearch({ host: `http://127.0.0.1:7700` })
-const add = await client.index(userId).addDocuments(actions)
-console.log(add)
-const res = await client.index(userId).search('search')
-console.log(res)
+const client = new MeiliSearch({ host: `http://127.0.0.1:7700` });
+const index = await client.getIndex(userId);
+await index.addDocuments(actions)
+
+await index.updateFilterableAttributes(["name", "description"]);
+const attributes = await index.getFilterableAttributes();
+
+console.log(attributes)
+const res = await index.search("d", { filter: ["name = delete"] });
+console.log(res);
+
+await client.index(userId).deleteAllDocuments()
